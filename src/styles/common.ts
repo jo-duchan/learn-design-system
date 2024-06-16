361e93e55afb8e935f6aa7b2ec005438e1c76e1c
jo-duchan <jo_duchan@icloud.com>
2024-06-16 12:10:29 +0900

    타이포 관련 세팅

--- src/styles/common.ts
+++ src/styles/common.ts
@@ -1,8 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
+const fontFamily =
+  '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif';
+
 const GlobalStyle = createGlobalStyle`
     ${reset};
+
+    html body * {
+        font-family: ${fontFamily};
+    }
+    
 `;
 
 export default GlobalStyle;
---
+++ src/styles/typography.ts
@@ -0,0 +1,33 @@
+import { css } from "styled-components";
+
+const Headline1 = css``;
+
+const Headline2 = css``;
+
+const Title1 = css``;
+
+const Title2 = css``;
+
+const Title3 = css``;
+
+const Body1 = css``;
+
+const Body2 = css``;
+
+const Caption = css``;
+
+/**
+ *
+ */
+const TextStyles = {
+  Headline1,
+  Headline2,
+  Title1,
+  Title2,
+  Title3,
+  Body1,
+  Body2,
+  Caption,
+};
+
+export default TextStyles;
